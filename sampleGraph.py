7f2a67bed628b80b902d5cc88fc797fc051b4bac
kmchord9 <kmchord9@gmail.com>
2020-11-18 14:27:42 +0900

    first commit

---
+++ sampleGraph.py
@@ -0,0 +1,42 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
+
+class GraphApp(App):
+
+    def __init__(self, *arg, **kwargs):
+        super().__init__(*arg, **kwargs)
+        self.title = 'Matplotlib graph on Kivy'
+
+    def build(self):
+        main_screen = BoxLayout()
+        main_screen.orientation = 'vertical'
+
+        label_text = 'The following is graph of Matplotlib'
+        label = Label(text=label_text)
+        label.size_hint_y = 0.2
+        main_screen.add_widget(label)
+
+        x = np.linspace(-np.pi, np.pi, 100)
+        y = np.sin(x)
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x,y)
+
+        main_screen.add_widget(fig.canvas)
+
+        return main_screen
+
+def main():
+    app = GraphApp()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
